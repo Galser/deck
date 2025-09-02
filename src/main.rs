07d06279dec58a3d94101a12078239db1ffc24f9
galser <guseletov@gmail.com>
2025-09-02 09:37:22 +0200

    Mut

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let suits = ["Hearts", "Spade", "Diamonds"];
+    let values = ["Ace", "Two", "Three"];
+    let mut cards = vec![];
+    for suit in suits { 
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+    let deck: Deck = Deck { cards };
+    println!("Here your deck: {:?}", deck);
 }
